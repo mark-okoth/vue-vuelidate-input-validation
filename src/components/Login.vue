598be80e1567dcb5fbad1dd62f23a3169da849c1
mark odhiambo okoth <markokoth96@gmail.com>
2020-02-25 15:36:19 +0300

    latest commit for the login page

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -3,14 +3,16 @@
     <div class="container">
       <div class="container">
         <h3>Login</h3>
-        <form>
+        <form @submit.prevent="sublition">
           <div class="input-field">
-            <input type="text" id="name" required />
-            <label for="name">Name</label>
+            <input type="email" id="name" :class="{invalid : $v.email.$error}" @blur="$v.email.$touch()" required v-model="email" />
+            <label for="name">Email</label>
+            <p class="red-text" v-if="!$v.email.email">please enter a valid email address</p>
+             <p class="red-text" v-if="!$v.email.required">you connot leave this input blank</p>
           </div>
           <div class="input-field">
-            <input type="text" id="email" required class="validate" />
-            <label for="email">Email</label>
+            <input type="password" id="password" :class="{invalid: $v.password.$error}" @blur="$v.password.$touch()" v-model="password" required />
+            <label for="password">Password</label>
           </div>
           <div class="input-field">
             <input type="submit" class="btn-small" />
@@ -23,10 +25,36 @@
 
 
 <script>
+import { required, minLength, email } from 'vuelidate/lib/validators'
 export default {
-  name: "Login"
+  name: "Login", 
+  data(){
+    return{
+     email: "",
+     password: ""
+    }
+  },
+  validations:{
+    email:{
+      required, 
+      email
+    },
+    password:{
+     minLen: minLength(6)
+    }
+  }
 };
 </script>
 
 <style>
+
+.input-field.invalid label {
+  color: red;
+}
+
+.input-field.invalid {
+  border: 1px solid red;
+  border-top: red;
+  background-color: #ffc9aa;
+}
 </style>
